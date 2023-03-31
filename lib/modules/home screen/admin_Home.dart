eb57a04cf2b2dce69e5506bb556d64378aa5809c
mo87654 <mk0316929@gmail.com>
2023-03-31 16:31:53 +0200

    auth & firestore

--- lib/modules/home screen/admin_Home.dart
+++ lib/modules/home screen/admin_Home.dart
@@ -1,5 +1,6 @@
 
 import 'package:admin_app/Provider/auth_Provider.dart';
+import 'package:admin_app/models/search_model.dart';
 import 'package:admin_app/modules/home%20screen/search_delegate.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
@@ -23,15 +24,21 @@ class _AdminHomeState extends State<AdminHome> {
   List names=[];
   List busIDs = [];
   List busDriver = [];
+
   getNames(String collection)async{
     names.clear();
     var dataref = FirebaseFirestore.instance.collection(collection);
     var response = await dataref.get();
     response.docs.forEach((element) {
       setState(() {
+        /*names.add(Student_Model(
+            name: element.data()['name'],
+            id: element.data()['id']
+        ));*/
         names.add(element.data()['name']);
       });
     });
+
   }
   getBusId()async{
     busIDs.clear();
@@ -44,10 +51,7 @@ class _AdminHomeState extends State<AdminHome> {
         busDriver.add(element.data()['Driver_name']);
       });
     });
-    print(busIDs);
-    print(busDriver);
   }
-
 
   @override
   void initState() {
