813f38aab5ffe50a3f9516e5e0f828bf46df84b6
Manarhamed00 <manar.hamed762001@gmail.com>
2023-07-07 16:37:36 +0300

    update my account page/ manar

--- lib/modules/my account screen/My_account.dart
+++ lib/modules/my account screen/My_account.dart
@@ -5,8 +5,10 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
+import 'package:path/path.dart';
+import 'package:path_provider/path_provider.dart';
 import 'package:shared_preferences/shared_preferences.dart';
-
+import 'dart:io';
 import '../../shared/component/SignoutMessage.dart';
 import '../../shared/component/colors.dart';
 import '../AboutUs Screen/AboutUs.dart';
@@ -44,6 +46,46 @@ class _MyAccountState extends State<MyAccount> {
 
   }
 
+  Future<void> savephoto(Path) async {
+    SharedPreferences saveimage = await SharedPreferences.getInstance();
+    saveimage.setString("imagepath", Path);
+  }
+  Future<String?> loadimage() async {
+    SharedPreferences saveimage = await SharedPreferences.getInstance();
+    setState(() {
+      _imagepath= saveimage.getString("imagepath");
+    });
+   // return saveimage.getString("imagepath");
+  }
+
+   Future<String?> loadimagedrawer() async {
+     SharedPreferences saveimage = await SharedPreferences.getInstance();
+    return saveimage.getString("imagepath");
+   }
+  void takePhoto(ImageSource source) async {
+    final pickedFile = await picker.pickImage(source: source);
+    if (pickedFile != null) {
+      final appDir = await getApplicationDocumentsDirectory();
+      final fileName = basename(pickedFile.path);
+      final savedImage = await File(pickedFile.path).copy('${appDir.path}/$fileName');
+      setState(() {
+        _imagepath = savedImage.path;
+        print(_imagepath);
+      });
+      savephoto(_imagepath!); // حفظ الصورة المحددة
+      final newImage = File(_imagepath!);
+      final imageBytes = newImage.readAsBytesSync();
+      final image64 = base64Encode(imageBytes);
+      print(image64);
+    }
+  }
+  void deletePhoto() async {
+    setState(() {
+      _imagepath = null;
+    });
+    SharedPreferences saveimage = await SharedPreferences.getInstance();
+    saveimage.remove("imagepath");
+  }
   final ImagePicker picker = ImagePicker();
   Color purple = const Color.fromRGBO(38, 107, 128, 0.9490196078431372);
   Color lpurplet = const Color.fromRGBO(0, 102, 128, 0.9490196078431372);
@@ -56,7 +98,7 @@ class _MyAccountState extends State<MyAccount> {
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
-
+          elevation: 0,
           backgroundColor: app_color(),
           leading:  Builder(
               builder: (context) {
@@ -78,7 +120,23 @@ class _MyAccountState extends State<MyAccount> {
             child: Column(
               children: [
                 ListTile(
-                  leading: const Icon(Icons.person),
+                  leading: FutureBuilder<String?>(
+                    future: loadimagedrawer(),
+                    builder: (BuildContext context, AsyncSnapshot<String?> snapshot) {
+                      if (snapshot.hasData && snapshot.data != null) {
+                        return CircleAvatar(
+                          radius: 30,
+                          backgroundImage: FileImage(File(snapshot.data!)),
+                        );
+                      } else {
+                        return CircleAvatar(
+                          radius: 50,
+                          child: Icon(Icons.person),
+                          backgroundColor: Colors.white,
+                        );
+                      }
+                    },
+                  ),
                   title: FutureBuilder(
                     future: getuserinfo(),
                     builder: (_ , AsyncSnapshot snapshot) {
@@ -266,7 +324,7 @@ class _MyAccountState extends State<MyAccount> {
                               child: Padding(
                                 padding: EdgeInsets.all(40),
                                 child: Text(
-                                  "Profile",
+                                  "",
                                   style: TextStyle(
                                     fontSize: 25,
                                     letterSpacing: 1.5,
@@ -287,11 +345,16 @@ class _MyAccountState extends State<MyAccount> {
 
                             _imagepath != null?
                                 CircleAvatar(backgroundImage: FileImage(File(_imagepath!)),radius: 80,)
-                            :CircleAvatar(
-                                radius: 64,
-                                backgroundImage: _imageFile == null ?
-                                AssetImage("assets/images/User3.jpg")
-                                    : FileImage(File(_imageFile!.path)) as ImageProvider),
+                                :    GestureDetector(
+                              onTap: () {
+                                takePhoto(ImageSource.gallery);
+                              },
+                              child: CircleAvatar(
+                                  radius: 64,
+                                  backgroundImage: _imageFile == null ?
+                                  AssetImage("assets/images/User3.jpg")
+                                      : FileImage(File(_imageFile!.path)) as ImageProvider),
+                            ),
 
                             CircleAvatar(
                               backgroundColor:  const Color(0xff4d6aaa),
@@ -314,21 +377,7 @@ class _MyAccountState extends State<MyAccount> {
                     ],
                   ),
                 ),
-                OutlinedButton.icon(
-                  icon: const Icon(Icons.save_as_outlined,
-                    color: Colors.black,
-                  ),
-                  onPressed: () {
-                    savephoto(_imageFile?.path);
-                    loadimage();
-                  },
-                  label: const Text("save",
-                    style: TextStyle(
-                      color: Colors.black,
 
-                    ),
-                  ),
-                ),
               ],
 
             ),
@@ -438,9 +487,9 @@ class _MyAccountState extends State<MyAccount> {
 
   Widget bottomSheet() {
     return Container(
-      color: const Color(0xff8093bc),
+      color: const Color(0xf2cad4f7),
       height: 100,
-      width: MediaQuery.of(context).size.width,
+      // width: MediaQuery.of(context).size.width,
       margin: const EdgeInsets.symmetric(
         horizontal: 20,
         vertical: 20,
@@ -491,6 +540,21 @@ class _MyAccountState extends State<MyAccount> {
                   ),
 
                 ),
+                OutlinedButton.icon(
+
+                  icon: const Icon(Icons.delete,
+                    color: Colors.black,
+                  ),
+                  onPressed: () {
+                    deletePhoto();
+                  },
+                  label: const Text("delete",
+                    style: TextStyle(
+                        color: Colors.black
+                    ),
+                  ),
+
+                ),
               ],
             ),
           )
@@ -499,43 +563,9 @@ class _MyAccountState extends State<MyAccount> {
     );
   }
 
-  void takePhoto(ImageSource source) async {
-    final pickedFile = await picker.getImage(
-      source: source,
-    );
-    setState(() {
-      _imageFile = pickedFile ?? '' as PickedFile;
-      print(_imageFile?.path);
-    });
-  }
-
-  pickPicture() async {
-    _imageFile = await picker.getImage(
-        source: ImageSource.gallery, maxHeight: 200, maxWidth: 200);
-
-    print(_imageFile);
-
-    if(_imageFile != null) {
-      final File newImage = File(_imageFile!.path);
-
-      List<int> imageBytes = newImage.readAsBytesSync();
-      image64 = base64Encode(imageBytes);
-    }
-  }
-
-  Future<void> savephoto(Path) async {
-    SharedPreferences saveimage = await SharedPreferences.getInstance();
- saveimage.setString("imagepath", Path);
 
 
-  }
 
-  Future<void> loadimage() async {
-    SharedPreferences saveimage = await SharedPreferences.getInstance();
-    setState(() {
-      _imagepath= saveimage.getString("imagepath");
-    });
-  }
 }
 class HeaderCurvedContainer extends CustomPainter {
   @override
